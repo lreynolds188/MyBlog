be1838a2b52adba15eadb9919d8370e727e94171
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2023-03-22 02:58:58 +1000

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,10 +4,10 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        My Journey
+        Blog
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        An inspirational perspective from a single woman's experiences in a strange and ever changing world.{' '}
+        A collection of articles on interesting topics from philosophy to politics designed to inform in a simple, honest, and educational manner.{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
